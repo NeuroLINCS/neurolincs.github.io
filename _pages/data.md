892e62b381ca30badca70054ebc258fdb40126a9
Alexander Lenail <alexander.lenail@tufts.edu>
2016-10-28 14:30:47 -0400

    add a bar chart to the data page.

--- _pages/data.md
+++ _pages/data.md
@@ -5,45 +5,43 @@ permalink: /data/
 
 <h1 class="custom__title">Our Data</h1>
 
-<section class="page__hero">
-
 <!-- <div class="custom__pie-chart"><canvas id="celltypes" width="10" height="10"></canvas></div> -->
-<div class="custom__pie-chart"><canvas id="diseases" width="10" height="10"></canvas></div>
+<div class="custom__chart custom__pie-chart"><canvas id="diseases" width="10" height="10"></canvas></div>
 <!-- <div class="custom__pie-chart"><canvas id="assays" width="10" height="10"></canvas></div> -->
 <!-- <div class="custom__pie-chart"><canvas id="filetypes" width="100" height="100"></canvas></div> -->
 <!-- <div class="custom__pie-chart"><canvas id="perturbations" width="100" height="100"></canvas></div> -->
-
-</section>
 
 The NeuroLINCS Center creates a wealth of cellular data that is patient-specific in the context of their baseline genetic perturbations and in the presence of other environmental stressors. Motor neurons (iMNs) are produced from induced pluripotent stem cell (iPSC) technologies and these cells are analyzed using epigenomics, transcriptomics, proteomics, and robotic imaging.
 
 Amyotrophic lateral sclerosis (ALS) is a severe neurodegenerative condition characterized by loss of motor neurons in the brain and spinal cord. Spinal muscular atrophy (SMA) is a recessively inherited pediatric neuromuscular disease characterized by degeneration of spinal motor neurons. The control (CTR) cells come from healthy individuals.
 
+<div class="custom__chart custom__bar-chart"><canvas id="releases" width="10" height="7"></canvas></div>
+
+---
 
 # Experiment 1
 
 ## iPSC - ALS, SMA and Control (unaffected) subject-derived iPSC lines.
-
-### WGS, RNA-Seq, ATAC-Seq and SWATH-MS were carried out on isolated mRNA, DNA and protein respectively obtained from induced Pluripotent Stem Cells (iPSC) cell lines. These lines were derived from ALS, SMA and Control (unaffected) individuals (three of each).
+WGS, RNA-Seq, ATAC-Seq and SWATH-MS were carried out on isolated mRNA, DNA and protein respectively obtained from induced Pluripotent Stem Cell (iPSC) lines. These lines were derived from ALS, SMA and Control (unaffected) individuals.  Multiple levels of data and analysis including full information on the number of replicates are available at the links below.
 
 - [RNA-Seq](http://lincsportal.ccs.miami.edu/datasets/#/view/LDS-1356)
 - [Proteomic – SWATH-MS](http://lincsportal.ccs.miami.edu/datasets/#/view/LDS-1358)
 - [ATAC-Seq](http://lincsportal.ccs.miami.edu/datasets/#/view/LDS-1354)
 
+---
 
 # Experiment 2
 
 ## Motor Neurons (iMNs) – ALS, SMA and Control (unaffected) subject motor neuron lines
-
 **iMN long differentiation protocol**
 
 Data release Spring of 2017
 
+---
 
 # Experiment 3
 
 ## Motor Neurons (iMNs) – ALS, SMA and Control (unaffected) subject motor neuron lines
-
 **iMN short differentiation protocol - mature neurons**
 
 Data release Fall of 2017
--- assets/js/animation.js
+++ assets/js/animation.js
@@ -99,9 +99,39 @@ $(document).ready(function() {
     },
     options: {
       title: { display: true, text: "Diseases", fontSize: 24 },
-      tooltips: {enabled: false}
+      tooltips: {enabled: false },
+      legend: { labels: { fontSize: 16 } }
     }
   });
+
+  var DataReleases = new Chart($("#releases"), {
+    type: "bar",
+    data: {
+      labels: ["9/16/2016", "6/21/2017", "12/21/2017"],
+      datasets: [
+        {data: [10, 20, 38], label: "Transcriptomic",
+        backgroundColor: ['rgba(54, 162, 235, 0.2)','rgba(54, 162, 235, 0.2)','rgba(54, 162, 235, 0.2)','rgba(54, 162, 235, 0.2)'],
+        borderColor: ['rgba(54, 162, 235, 1)','rgba(54, 162, 235, 1)','rgba(54, 162, 235, 1)','rgba(54, 162, 235, 1)'], borderWidth: 1},
+        {data: [9, 19, 37], label: "Epigenomic",
+        backgroundColor: ['rgba(255, 159, 64, 0.2)','rgba(255, 159, 64, 0.2)','rgba(255, 159, 64, 0.2)','rgba(255, 159, 64, 0.2)'],
+        borderColor: ['rgba(255, 159, 64, 1)','rgba(255, 159, 64, 1)','rgba(255, 159, 64, 1)','rgba(255, 159, 64, 1)'], borderWidth: 1},
+        {data: [10, 20, 38], label: "Proteomic",
+        backgroundColor: ['rgba(153, 102, 255, 0.2)','rgba(153, 102, 255, 0.2)','rgba(153, 102, 255, 0.2)','rgba(153, 102, 255, 0.2)'],
+        borderColor: ['rgba(153, 102, 255, 1)','rgba(153, 102, 255, 1)','rgba(153, 102, 255, 1)','rgba(153, 102, 255, 1)'], borderWidth: 1},
+        {data: [0, 19, 28], label: "Imaging",
+        backgroundColor: ['rgba(255, 99, 132, 0.2)','rgba(255, 99, 132, 0.2)','rgba(255, 99, 132, 0.2)','rgba(255, 99, 132, 0.2)'],
+        borderColor: ['rgba(255,99,132,1)','rgba(255,99,132,1)','rgba(255,99,132,1)','rgba(255,99,132,1)'], borderWidth: 1 }
+      ]
+    },
+    options: {
+      title: { display: true, text: "Data Releases", fontSize: 24 },
+      scales: {xAxes: [{ stacked: true }], yAxes: [{ stacked: true }] },  // Convoluted way of saying "stacked bar chart please"
+      tooltips: {enabled: false},
+      legend: { labels: { fontSize: 16 } }
+    }
+  });
+
+
 
   // var Assays = new Chart($("#assays"), {
   //   type: "doughnut",
@@ -124,217 +154,3 @@ $(document).ready(function() {
 
 
 
-
-
-//   var ctx = $("#myChart");
-//   var myChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//       labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//       datasets: [{
-//         label: '# of Votes',
-//         data: [12, 19, 3, 5, 2, 3],
-//         backgroundColor: [
-//           'rgba(255, 99, 132, 0.2)',
-//           'rgba(54, 162, 235, 0.2)',
-//           'rgba(255, 206, 86, 0.2)',
-//           'rgba(75, 192, 192, 0.2)',
-//           'rgba(153, 102, 255, 0.2)',
-//           'rgba(255, 159, 64, 0.2)'
-//         ],
-//         borderColor: [
-//           'rgba(255,99,132,1)',
-//           'rgba(54, 162, 235, 1)',
-//           'rgba(255, 206, 86, 1)',
-//           'rgba(75, 192, 192, 1)',
-//           'rgba(153, 102, 255, 1)',
-//           'rgba(255, 159, 64, 1)'
-//         ],
-//         borderWidth: 1
-//       }]
-//     },
-//     options: {
-//       scales: {
-//         yAxes: [{
-//           ticks: {
-//             beginAtZero:true
-//           }
-//         }]
-//       }
-//     }
-//   });
-
-
-// (function() {
-//   // 9
-//   // 9,999
-//   // 99 999
-//   // 99.9
-//   var MATCH_NUMBER = /[0-9, ]+(\.\d*)?/;
-//   var CHART_HEIGHT = 400;
-//   var unique_id = 0;
-
-//   function color(n) {
-//     return ([
-//       {
-//         fillColor:   "rgba(174, 199, 232, 0.5)",
-//         strokeColor: "rgba( 31, 119, 180, 1)",
-//         pointColor:  "rgba( 31, 119, 180, 1)",
-//         pointStrokeColor: "#fff",
-//       },
-//       {
-//         fillColor:   "rgba(255, 187, 120, 0.5)",
-//         strokeColor: "rgba(255, 127,  14, 1)",
-//         pointColor:  "rgba(255, 127,  14, 1)",
-//         pointStrokeColor: "#fff",
-//       },
-//       {
-//         fillColor:   "rgba(152, 223, 138, 0.5)",
-//         strokeColor: "rgba( 44, 160,  44, 1)",
-//         pointColor:  "rgba( 44, 160,  44, 1)",
-//         pointStrokeColor: "#fff",
-//       },
-//       {
-//         fillColor:   "rgba(255,  39,  40, 0.5)",
-//         strokeColor: "rgba(214,  39,  40, 1)",
-//         pointColor:  "rgba(214,  39,  40, 1)",
-//         pointStrokeColor: "#fff",
-//       },
-//       {
-//         fillColor:   "rgba(197, 176, 213, 0.5)",
-//         strokeColor: "rgba(148, 103, 189, 1)",
-//         pointColor:  "rgba(148, 103, 189, 1)",
-//         pointStrokeColor: "#fff",
-//       },
-//     ])[n] || {};
-//   }
-
-//   function get_data(container) {
-//     // Fail unless the next element in the DOM is the data table.
-//     var table = container.nextElementSibling;
-//     var series_shift = 2;
-//     var axis_selector = 'td:nth-child(1)';
-//     var label_selector = 'th:nth-child(N)';
-//     var series_selector = 'td:nth-child(N)';
-//     if (container.dataset.series == "rows") {
-//       series_shift = 1;
-//       axis_selector = 'th:not(:nth-child(1))';
-//       label_selector = 'tbody tr:nth-child(N) :first-child';
-//       series_selector = 'tbody tr:nth-child(N) :not(:first-child)';
-//     }
-
-//     // Assume the columns form series.
-//     var labels =
-//       Array.prototype.slice.call(table.querySelectorAll(axis_selector), 0)
-//       .map(function(e) { return e.textContent; });
-//     var datasets = [];
-//     for (var N = 0; N < 5; N++) {
-//       var series_title =
-//         table.querySelector(label_selector.replace('N', N + series_shift));
-//       if (!series_title) {
-//         break;
-//       }
-//       var series_data =
-//         Array.prototype.slice.call(table.querySelectorAll(series_selector.replace('N', N + series_shift)), 0)
-//         .map(function(e) {
-//           var match = MATCH_NUMBER.exec(e.textContent) || [ '' ];
-//           return parseFloat(match[0].replace(/[^0-9.]/g, ''));
-//         });
-//       var series = color(N);
-//       series.data = series_data;
-//       series.title = series_title.textContent;
-//       datasets.push(series);
-//     }
-//     return {
-//       labels: labels,
-//       datasets: datasets,
-//       table: table,
-//     };
-//   }
-
-//   function make_chart(container) {
-//     var id = "chart_" + ++unique_id;
-//     container.id = id;
-//     container.className = "figure";
-
-//     var focus_table = container.dataset.focus != "chart";
-//     var data = get_data(container);
-//     var options = {
-//       scaleFontFamily: 'Roboto',
-//       scaleFontSize: 14,
-//       showLegend: true,
-//     };
-//     for (var k in container.dataset) {
-//       options[k] = container.dataset[k];
-//     }
-//     var renderer = 'Bar';
-//     if (container.dataset.type == "line") {
-//       renderer = 'Line';
-//     }
-
-//     var tabs = document.createElement('div');
-//     tabs.className = "tabs";
-
-//     var view_table = document.createElement('input');
-//     var view_table_label = document.createElement('label');
-//     view_table.type = 'radio';
-//     view_table.name = 'view_' + id;
-//     view_table.className = view_table_label.className = "view_table";
-//     view_table.id = view_table_label.htmlFor = 'view_' + id + '_table';
-//     view_table.checked = focus_table;
-//     view_table_label.textContent = "Table";
-//     container.appendChild(view_table);
-//     tabs.appendChild(view_table_label);
-
-//     var view_chart = document.createElement('input');
-//     var view_chart_label = document.createElement('label');
-//     view_chart.type = 'radio';
-//     view_chart.name = 'view_' + id;
-//     view_chart.className = view_chart_label.className = "view_chart";
-//     view_chart.id = view_chart_label.htmlFor = 'view_' + id + '_chart';
-//     view_chart.checked = !focus_table;
-//     view_chart_label.textContent = "Chart";
-//     container.appendChild(view_chart);
-//     tabs.appendChild(view_chart_label);
-
-//     container.appendChild(tabs);
-//     container.appendChild(data.table);
-
-//     var canvas = document.createElement("canvas");
-//     canvas.width = container.clientWidth;
-//     canvas.height = Math.min(CHART_HEIGHT, document.body.clientHeight - 20);
-//     container.appendChild(canvas);
-
-//     var rendered = false;
-//     function render() {
-//       var new_width = container.clientWidth;
-//       var new_height = Math.min(CHART_HEIGHT, document.body.clientHeight - 20);
-
-//       if (rendered && canvas.width == new_width && canvas.height == new_height) {
-//         return;
-//       }
-//       rendered = true;
-
-//       canvas.width = new_width;
-//       canvas.height = new_height;
-//       var ctx = canvas.getContext('2d');
-//       var myChart = new Chart(ctx)[renderer](data, options);
-//     }
-
-//     if (!focus_table) {
-//       render();
-//     }
-//     view_chart.addEventListener('change', render, false);
-//     window.addEventListener('resize', function() {
-//       if (view_chart.checked) {
-//         render();
-//       }
-//     }, false);
-//   }
-
-//   window.addEventListener('load', function() {
-//     Array.prototype.slice.call(document.getElementsByTagName("div"), 0)
-//       .filter(function(e) { return e.dataset.figure === 'chart'; })
-//       .map(make_chart);
-//   }, false);
-// }());
